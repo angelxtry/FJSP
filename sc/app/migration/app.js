a0a332add288ed5423d626d521be39442e652f1e
angelxtry <angelxtry@gmail.com>
2018-06-18 21:37:21 +0900

    first commit

---
+++ sc/app/migration/app.js
@@ -0,0 +1,12 @@
+require('../../module/back/root/Orm.js');
+
+console.log('마이그레이션!');
+
+(async () => {
+  const { SELECT, FROM, WHERE } = Orm;
+  const { query } = await Orm.connect();
+  const res = await query(
+    SELECT `*`, FROM `pg_tables`, WHERE `schemaname = 'public' and tablename = 'migrations'`
+  );
+  console.log(res);
+}) ();
---
+++ sc/module/back/root/Orm.js
@@ -0,0 +1,46 @@
+require('../../share/root/Functional')
+!function(Root) {
+  const { reduce } = Functional;
+  const { Pool } = require('pg');
+  function query(pool, qs) {
+    // console.log(qs);
+    // console.log(reduce((a, b) => `${a} ${b}`, qs));
+    return pool.query(reduce((a, b) => `${a} ${b}`, qs));
+  }
+
+  async function connect(info) {
+    const pool = await new Pool(
+      // {
+      // user: 'leesh',
+      // host: 'localhost',
+      // database: 'sc',
+      // password: '',
+      // port: 5432,
+    // }
+  );
+    return {
+      query(...qs) {
+        return query(pool, qs);
+      },
+      transaction() {
+
+      }
+    };
+  }
+
+  function SELECT(strs, ...vals) {
+    return `SELECT $(strs[0])`;
+  }
+  function FROM(strs, ...vals) {
+    return `FROM $(strs[0])`;
+  }
+  function WHERE(strs, ...vals) {
+    return `WHERE $(strs[0])`;
+  }
+  Root.Orm = {
+    connect, 
+    SELECT,
+    FROM,
+    WHERE,
+  };
+} (global);
